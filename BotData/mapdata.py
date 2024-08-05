14e33c3d92d528ca628a25a0ffa1c82acafde78f
Catotron <101963814+CatotronExists@users.noreply.github.com>
2024-08-06 09:00:28 +1000

    Apex Proximity Chat Release (Part 3/3)

---
+++ BotData/mapdata.py
@@ -0,0 +1,324 @@
+MapData = {
+    "Worlds Edge": {
+        "Big Maude": {
+            "POICenter": [13234,12000]
+        },
+        "Climatizer": {
+            "POICenter": [11827,3114]
+        },
+        "Countdown": {
+            "POICenter": [4735,5865]
+        },
+        "Fragment": {
+            "POICenter": [10275,7106]
+        },
+        "Harvester": {
+            "POICenter": [7620,10227]
+        },
+        "Landslide": {
+            "POICenter": [5843,7533]
+        },
+        "Launch Site": {
+            "POICenter": [9035,14824]
+        },
+        "Lava Fissure": {
+            "POICenter": [3172,6108]
+        },
+        "Lava Siphon": {
+            "POICenter": [9489,12013]
+        },
+        "Mirage à Trois": {
+            "POICenter": [3135,8256]
+        },
+        "Monument": {
+            "POICenter": [8864,6166]
+        },
+        "Overlook": {
+            "POICenter": [13718,6445]
+        },
+        "Skyhook": {
+            "POICenter": [4509,4561]
+        },
+        "Stacks": {
+            "POICenter": [12503,13373]
+        },
+        "Staging": {
+            "POICenter": [5086,9550]
+        },
+        "Survey Camp": {
+            "POICenter": [10571,3377]
+        },
+        "The Dome": {
+            "POICenter": [11811,15201]
+        },
+        "The Epicenter": {
+            "POICenter": [10326,4558]
+        },
+        "The Geyser": {
+            "POICenter": [12291,9903]
+        },
+        "The Tree": {
+            "POICenter": [7042,14402]
+        },
+        "Thermal Station": {
+            "POICenter": [4547,11971]
+        },
+        "Trials": {
+            "POICenter": [4016,4088]
+        },
+    }, 
+    "Olympus": { # DATA IS MISSING, WHY? VISIT: https://github.com/CatotronExists/Apex-Prox-Chat#notice
+        "Bonsai Plaza": {
+            "POICenter": [0,0]
+        },
+        "Clinc": {
+            "POICenter": [0,0]
+        },
+        "Docks": {
+            "POICenter": [0,0]
+        },
+        "Elysium": {
+            "POICenter": [0,0]
+        },
+        "Energy Depot": {
+            "POICenter": [0,0]
+        },
+        "Estates": {
+            "POICenter": [0,0]
+        },
+        "Fight Night": {
+            "POICenter": [0,0]
+        },
+        "Gardens": {
+            "POICenter": [0,0]
+        },
+        "Grow Towers": {
+            "POICenter": [0,0]
+        },
+        "Hammond Labs": {
+            "POICenter": [0,0]
+        },
+        "Hydroponics": {
+            "POICenter": [0,0]
+        },
+        "Icarus": {
+            "POICenter": [0,0]
+        },
+        "Oasis": {
+            "POICenter": [0,0]
+        },
+        "Orbital Cannon": {
+            "POICenter": [0,0]
+        },
+        "Phase Driver": {
+            "POICenter": [0,0]
+        },
+        "Power Grid": {
+            "POICenter": [0,0]
+        },
+        "Rift": {
+            "POICenter": [0,0]
+        },
+        "Solar Array": {
+            "POICenter": [0,0]
+        },
+        "Supercarrier": {
+            "POICenter": [0,0]
+        },
+        "Terminal": {
+            "POICenter": [0,0]
+        },
+        "Turbine": {
+            "POICenter": [0,0]
+        },
+    },
+    "Kings Canyon": {
+        "Airbase": {
+            "POICenter": [3329,8551]
+        },
+        "Artillery": {
+            "POICenter": [9405,2306]
+        },
+        "Basin": {
+            "POICenter": [13265,3065]
+        },
+        "Bunker": {
+            "POICenter": [6223,7115]
+        },
+        "Capacitor": {
+            "POICenter": [13078,6184]
+        },
+        "Crash Site": {
+            "POICenter": [6506,1011]
+        },
+        "Containment": {
+            "POICenter": [7189,4571]
+        },
+        "Gauntlet": {
+            "POICenter": [3930,10557]
+        },
+        "Hillside" : {
+            "POICenter": [8994,6479]
+        },
+        "Hydro Dam": {
+            "POICenter": [13491,9252]
+        },
+        "Map Room": {
+            "POICenter": [13109,12774]
+        },
+        "Market": {
+            "POICenter": [8486,10349]
+        },
+        "Relic": {
+            "POICenter": [6194,11299]
+        },
+        "Repulsor": {
+            "POICenter": [12682,11194]
+        },
+        "Rig": {
+            "POICenter": [15221,3778]
+        },
+        "Runoff": {
+            "POICenter": [3466,6116]
+        },
+        "Caustic Treatment": {
+            "POICenter": [9655,13884]
+        },
+        "Spotted Lakes": {
+            "POICenter": [5335,1667]
+        },
+        "Swamps": {
+            "POICenter": [15135,7892]
+        },
+        "The Cage": {
+            "POICenter": [11202,8456]
+        },
+        "The Pit": {
+            "POICenter": [4691,4910]
+        },
+        "Labs": {
+            "POICenter": [-999999,-999999] ### MISSING
+        },
+    },
+    "Broken Moon": {
+        "Alpha Base": {
+            "POICenter": [9078,2878]
+        },
+        "Space Port": {
+            "POICenter": [1604,4267]
+        },
+        "Underpass": {
+            "POICenter": [3444,9440]
+        },
+        "Breaker Wharf": {
+            "POICenter": [12691,4267]
+        },
+        "Dry Gulch": {
+            "POICenter": [2416,6096]
+        },
+        "Eternal Gardens": {
+            "POICenter": [12778,5821]
+        },
+        "Stasis Array": {
+            "POICenter": [10442,10301]
+        },
+        "The Core": {
+            "POICenter": [4914,4764]
+        },
+        "Production Yard": {
+            "POICenter": [1838,8896]
+        },
+        "Quarantine Zone": {
+            "POICenter": [6568,8913]
+        },
+        "Solar Pods": {
+            "POICenter": [4376,13726]
+        },
+        "Cliff Side": {
+            "POICenter": [8808,6261]
+        },
+        "Hazmat Tunnel": {
+            "POICenter": [8905,9367]
+        },
+        "The Foundry": {
+            "POICenter": [2711,14195]
+        },
+        "Cultivation": {
+            "POICenter": [5997,12695]
+        },
+        "Atmostation": {
+            "POICenter": [10090,13742]
+        },
+        "Terraformer": {
+            "POICenter": [9180,5116]
+        },
+        "The Divide": {
+            "POICenter": [14115,8547]
+        },
+        "Experimental Labs": {
+            "POICenter": [1792,11750]
+        },
+        "Bionomics": {
+            "POICenter": [13720,12783]
+        },
+    },
+    "Storm Point": {
+        "Barometer": {
+            "POICenter": [5630,11712]
+        },
+        "Cascade Falls": {
+            "POICenter": [8223,6107]
+        },
+        "Cenote Cave": {
+            "POICenter": [2827,11245]
+        },
+        "Ceto Station": {
+            "POICenter": [6585,8763]
+        },
+        "Checkpoint": {
+            "POICenter": [5084,5037]
+        },
+        "Coastal Camp": {
+            "POICenter": [-999999,-999999] ### MISSING
+        },
+        "Command Center": {
+            "POICenter": [10418,4809]
+        },
+        "Devastated Coast": {
+            "POICenter": [13144,12321]
+        },
+        "Downed Beast": {
+            "POICenter": [1869,5362]
+        },
+        "Echo HQ": {
+            "POICenter": [12853,12386]
+        },
+        "Jurassic": {
+            "POICenter": [9209,11374]
+        },
+        "Launch Pad": {
+            "POICenter": [12967,10120]
+        },
+        "Lighting Rod": {
+            "POICenter": [13054,3543]
+        },
+        "North Pad": {
+            "POICenter": [4159,2528]
+        },
+        "Storm Catcher": {
+            "POICenter": [12288,6390]
+        },
+        "The Mill": {
+            "POICenter": [2918,10778]
+        },
+        "The Pylon": {
+            "POICenter": [9209,11374]
+        },
+        "The Wall": {
+            "POICenter": [9125,2383]
+        },
+        "Zeus Station": {
+            "POICenter": [13048,3538]
+        },
+    }
+}
